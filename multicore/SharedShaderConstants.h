85efc5e67be3793608863fed40b1e3764988cb65
David Danielsson <daviddanielsson.dd@gmail.com>
2015-10-18 00:24:52 +0200

    Cool header files

---
+++ multicore/SharedShaderConstants.h
@@ -0,0 +1,16 @@
+#ifndef SharedShaderConstants_h__
+#define SharedShaderConstants_h__
+
+static const int MAX_SPHERES = 2047;
+const static int MAX_POINT_LIGHTS = 10;
+
+const static int MAX_TRIANGLES = 1024;
+const static int MAX_VERTICES = MAX_TRIANGLES * 3;
+const static int MAX_INDICIES = MAX_TRIANGLES * 3;
+
+static const float FLOAT_MAX = 3.4e38f;
+
+static const float SCREEN_RES_X = 1280.0f;
+static const float SCREEN_RES_Y = 720.0f;
+
+#endif // SharedShaderConstants_h__
